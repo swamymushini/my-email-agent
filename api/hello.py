07e806d40c8e568a6c7d3e020e1d29c1fed8fb6f
gmushini <gmushini@rocketsoftware.com>
2025-09-07 23:19:56 +0530

    updated changes

--- api/hello.py
+++ api/hello.py
@@ -3,7 +3,6 @@ from http.server import BaseHTTPRequestHandler
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-Type', 'text/plain')
         self.end_headers()
-        self.wfile.write(b'Hey Im Gopal')
+        self.wfile.write('Hey Im Gopal'.encode())
