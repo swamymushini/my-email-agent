96e5260cc678a60393b9a785f0bfc0fc1cfece91
gmushini <gmushini@rocketsoftware.com>
2025-09-07 23:01:08 +0530

    Add simple hello API endpoint

---
+++ api/hello.py
@@ -0,0 +1,9 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        self.wfile.write(b'Hey Im Gopal')
